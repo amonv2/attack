82f3fb238f6fbc2329a847e55554b7503be0e49d
SalmonPlays <99386377+amonv2@users.noreply.github.com>
2022-07-21 12:29:01 +0100

    r

--- like_button.js
+++
@@ -1,5 +0,0 @@
-// ... the starter code you pasted ...
-
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
